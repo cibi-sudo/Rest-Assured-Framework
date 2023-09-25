6dc8bdd9a255eb6fd8db9ce50eea6c74bc31a6d7
cibi-sudo <cibiccf437@gmail.com>
2023-09-25 13:05:35 +0530

    Some minor changes

--- src/test/java/endpoints/UserEndPoints.java
+++ src/test/java/endpoints/UserEndPoints.java
@@ -7,21 +7,38 @@ import static io.restassured.RestAssured.given;
 
 public class UserEndPoints {
     public static Response createUser(User payload) {
-        Response response = given().contentType("application/json").accept("application/json").body(payload).when().post(Routes.postUrl);
+        Response response = given()
+                .contentType("application/json")
+                .accept("application/json")
+                .body(payload)
+                .when()
+                .post(Routes.postUrl);
         return response;
     }
     public static Response getUser(String username){
-        Response response = given().pathParam("username",username).when().get(Routes.getUrl);
+        Response response = given()
+                .pathParam("username",username)
+                .when()
+                .get(Routes.getUrl);
         return response;
     }
 
     public static Response updateUser(User payload, String username){
-        Response response = given().contentType("application/json").accept("application/json").body(payload).pathParam("username",username).when().put(Routes.putUrl);
+        Response response = given()
+                .contentType("application/json")
+                .accept("application/json")
+                .body(payload)
+                .pathParam("username",username)
+                .when()
+                .put(Routes.putUrl);
         return response;
     }
 
     public static Response deleteUser(String username){
-        Response response = given().pathParam("username",username).when().delete(Routes.deleteUrl);
+        Response response = given()
+                .pathParam("username",username)
+                .when()
+                .delete(Routes.deleteUrl);
         return response;
     }
 }
